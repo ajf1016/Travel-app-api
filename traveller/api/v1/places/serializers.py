b6ba0fe6f09edcbae7821f4be18b05303121ece8
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-04-12 14:41:38 +0530

    Gallery Images in Detail API

--- traveller/api/v1/places/serializers.py
+++ traveller/api/v1/places/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from places.models import Place
+from places.models import Place,Gallery
 from rest_framework import serializers
 
 
@@ -9,11 +9,27 @@ class PlaceSerializer(ModelSerializer):
         fields = ("id","name","featured_image","place")
         
         
+class GallerySerializer(ModelSerializer):
+    class Meta:
+        model = Gallery
+        fields = ("id","image")
+        
+        
 class PlaceDetailSerializer(ModelSerializer):
     category = serializers.SerializerMethodField()
+    gallery = serializers.SerializerMethodField()
+    
     class Meta:
         model = Place
         fields = "__all__"
     
     def get_category(self,instance):
         return instance.category.name
+    
+    def get_gallery(self,instance):
+        context = {
+            "request" : self.context.get('request')
+        }
+        images = Gallery.objects.filter(place=instance)
+        serializers = GallerySerializer(images,many=True,context=context)
+        return serializers.data
