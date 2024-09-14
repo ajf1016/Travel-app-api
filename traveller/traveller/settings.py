3519a5eecf67d4df35fb7f5571423f50f8866f36
Mohammed Ajmal Fayiz <83690916+ajf1016@users.noreply.github.com>
2024-09-14 10:17:31 +0530

    Update settings.py

--- traveller/traveller/settings.py
+++ traveller/traveller/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 #load .env variables
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
